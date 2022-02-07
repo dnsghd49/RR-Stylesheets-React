8746e307ede08816b69a07582373791374c5e2ee
Aaron Lu <aaronnoh0409@gmail.com>
2022-02-07 18:40:53 -0500

    messing around with css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import Whaaaa from './messingAround';
 
 function App() {
   const styledText = {
     fontSize: "25px",
     textAlign: "center",
     margin: "25",
-    border: "1px solid green"
+    border: "1px solid purple"
   };
 
   return (
     <div style={styledText}>
       <h1>Hello World</h1>
+      <Whaaaa styledText={styledText}/>
     </div>
   );
 }
---
+++ src/messingAround.js
@@ -0,0 +1,15 @@
+function Whaaaa(props) {
+    const tes3ttt = {
+        margin: "150px",
+        padding: "50px",
+        color: "white"
+    };
+
+    return (
+        <div style={tes3ttt}>
+            <h2 style={props.styledText}>ur really funny</h2>
+        </div>
+    )
+}
+
+export default Whaaaa
